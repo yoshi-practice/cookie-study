93802a4b69a81b9c16f65be6b63df8643273f702
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-08-07 12:00:00 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.end('hi');
+});
+const port = 8000;
+server.listen(port, () => {
+  console.info('Listening on' + port);
+})
