5b4365f21afa35e9cfe01e8d8eeeff0a8a6a60b7
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-12-14 12:00:00 +0900

    Update nowtime

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const http = require('http');
 const server = http.createServer((req, res) => {
+  const now = Date.now();
+  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
   res.end('hi');
 });
 const port = 8000;
