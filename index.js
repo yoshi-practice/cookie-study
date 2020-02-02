303870f3f18d998141536edf988e49e0dc5b1539
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-02-02 12:00:00 +0900

    Update ms datetime

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
   res.setHeader('Set-Cookie', 'last_access=' + now + ';');
-  res.end(req.headers.cookie);
+  const lastAccessTime = req.headers.cookie
+    ? parseInt(req.headers.cookie.split('last_access=')[1]) // できたとき (true)
+    : now; // できないとき（false)
+  res.end(new Date(lastAccessTime).toString());
 });
 const port = 8000;
 server.listen(port, () => {
