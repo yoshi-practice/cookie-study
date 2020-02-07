99c920800f2b81b908db12751f0362ce2eb7ae42
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-02-07 12:00:00 +0900

    Add expire date

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
+  res.setHeader('Set-Cookie', 'last_access=' + now + ';expires=Mon, 07 Jan 2036 00:00:00 GMT;');
   const lastAccessTime = req.headers.cookie
     ? parseInt(req.headers.cookie.split('last_access=')[1]) // できたとき (true)
     : now; // できないとき（false)
